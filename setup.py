fcfc95d0e624c341d5ef321d14e774aebcb5ce89
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2019-06-03 15:06:06 +0100

    Add a s3_media_upload script

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
     zip_safe=False,
     url="https://github.com/matrix-org/synapse-s3-storage-provider",
     py_modules=['s3_storage_provider'],
+    scripts=['scripts/s3_media_upload'],
     install_requires=[
-        'boto3'
+        'boto3>=1.9.23<2.0',
+        'botocore>=1.12.23<2.0',
+        'humanize>=0.5.1<0.6',
+        'psycopg2-binary>=2.7.5<3.0',
+        'PyYAML>=3.13<4.0',
+        'tqdm>=4.26.0<5.0'
     ],
 )
