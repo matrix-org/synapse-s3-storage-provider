e762c859ac7910e043f09f45a657ff607c66a599
David Robertson <davidr@element.io>
2022-10-12 11:51:55 +0100

    Fix dependencies syntax (#80)

--- setup.py
+++ setup.py
@@ -21,12 +21,12 @@ setup(
     py_modules=["s3_storage_provider"],
     scripts=["scripts/s3_media_upload"],
     install_requires=[
-        "boto3>=1.9.23<2.0",
-        "botocore>=1.12.23<2.0",
-        "humanize>=0.5.1<0.6",
-        "psycopg2>=2.7.5<3.0",
-        "PyYAML>=3.13<4.0",
-        "tqdm>=4.26.0<5.0",
+        "boto3>=1.9.23,<2.0",
+        "botocore>=1.12.23,<2.0",
+        "humanize>=0.5.1,<0.6",
+        "psycopg2>=2.7.5,<3.0",
+        "PyYAML>=3.13,<4.0",
+        "tqdm>=4.26.0,<5.0",
         "Twisted",
     ],
 )
