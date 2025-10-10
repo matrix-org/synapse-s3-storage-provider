69d213225c7c5e76f8e328a5ffa62f7722de9a4f
Andrew Morgan <andrew@amorgan.xyz>
2025-10-10 13:33:05 +0100

    Update minimum requirement for boto deps to 1.36.0
    This became required after
    https://github.com/matrix-org/synapse-s3-storage-provider/pull/127 made
    use of config options only added in 1.36.0.

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     py_modules=["s3_storage_provider"],
     scripts=["scripts/s3_media_upload"],
     install_requires=[
-        "boto3>=1.9.23,<2.0",
-        "botocore>=1.31.62,<2.0",
+        "boto3>=1.36.0,<2.0",
+        "botocore>=1.36.0,<2.0",
         "humanize>=4.0,<5.0",
         "psycopg2>=2.7.5,<3.0",
         "PyYAML>=5.4,<7.0",
